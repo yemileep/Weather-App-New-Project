55f1c96a89095b98c62b88fb5ebe3a8eb90fef34
yemileep <82692017+yemileep@users.noreply.github.com>
2022-07-11 20:28:42 -0500

    I styled the css

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 function displayTemperature(response) {
-
-let temperatureElement= document.querySelector("#temperature")
-temperatureElement.innerHTML=Math.round (response.data.main.temp);
-}
-
-let apiKey ="5f472b7acba333cd8a035ea85a0d4d4c";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}$units=metrict`;
-
-axios.get (url).then(displayTemperature);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  }
+  
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metric    `;
+  
+  axios.get(apiURL).then(displayTemperature);
