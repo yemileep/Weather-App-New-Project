11f5b9741b54f3fea6f568bc830034f145d78b63
yemileep <82692017+yemileep@users.noreply.github.com>
2022-08-08 11:15:52 -0500

    Added icon element

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ let descriptionElement=document.querySelector("#description");
 let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
+let iconElement=document.querySelector("#icon");
 
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
@@ -41,16 +42,17 @@ humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML= Math.round(response.data.wind.speed);
 dateElement.innerHTML= formatDate(response.data.dt *1000);
 iconElement.setAttribute(
-    "src",
+     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 );
-iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
 
 function search(city) {
     let apiKey ="5f472b7acba333cd8a035ea85a0d4d4c";
-    let city ="Paris";
+    let city ="New York";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 }
