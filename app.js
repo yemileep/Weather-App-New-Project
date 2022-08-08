e4af0f905441e434f72de54ea85c3359b338d8bd
yemileep <82692017+yemileep@users.noreply.github.com>
2022-08-08 10:49:26 -0500

    Added  the search engine

--- app.js
+++ app.js
@@ -1,10 +1,69 @@
-function displayTemperature(response) {
-
-let temperatureElement= document.querySelector("#temperature")
-temperatureElement.innerHTML=Math.round (response.data.main.temp);
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours= date.getHours();
+if (hours  < 10) {
+    hours = `0${hours}`;
 }
 
-let apiKey ="5f472b7acba333cd8a035ea85a0d4d4c";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}$units=metrict`;
+let minutes= date.getMinutes();
+if (minutes  < 10) {
+    minutes = `0${minutes}`;
+}
+let days= [
+"Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday"
+];
+let day= days[date.getDay()];
+    return`${day} ${hours}:${minutes}`;
+}
 
-axios.get (url).then(displayTemperature);
+
+
+
+function displayTemperature(response) {
+
+let temperatureElement= document.querySelector("#temperature");
+let cityElement=document.querySelector("#city");
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+let windElement=document.querySelector("#wind");
+let dateElement=document.querySelector("#date");
+
+temperatureElement.innerHTML=Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML= response.data.weather[0].description;
+humidityElement.innerHTML= response.data.main.humidity;
+windElement.innerHTML= Math.round(response.data.wind.speed);
+dateElement.innerHTML= formatDate(response.data.dt *1000);
+iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
+iconElement.setAttribute("alt", response.data.weather[0].description);
+
+}
+
+function search(city) {
+    let apiKey ="5f472b7acba333cd8a035ea85a0d4d4c";
+    let city ="Paris";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    console.log(cityInputElement.value);
+    
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
