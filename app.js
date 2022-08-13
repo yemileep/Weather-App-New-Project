c144ea76a03c189c6aaf4488066e330eb5902d93
yemileep <82692017+yemileep@users.noreply.github.com>
2022-08-13 17:21:13 -0500

    Update app js

--- app.js
+++ app.js
@@ -21,30 +21,41 @@ let days= [
 let day= days[date.getDay()];
     return`${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp){
+let date = new Date(time *1000);
+let day = date.getDay();
+let days =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day];
+
+}
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector(".weather-forecast");
   
     let forecastHTML = `<div class ="row">`;
   
     let days = ["Thu", "Fri", "Sat", "Sun"];
-    days.forEach(function (day) {
-      forecastHTML =
+    days.forEach(function (forecastDay, index) {
+if (index < 6 ) {
+
+}
+        forecastHTML =
         forecastHTML +
         `
   <div class="col-2">
-     <div class="weather-forecast-date">${day}</div> 
-  <img src="http://openweathermap.org/img/wn/03d@2x.png"
+     <div class="weather-forecast-date">${forecastDay.dt}</div> 
+  <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
   alt=""
   width="42"
   
   />
   <div class="weather-forecast-temperatures">
       <span class="weather-forecast-temperature-max"> 
-      18° 
+      ${Math.round(forecastDay.temp.max)}° 
       </span>
   <span class="weather-forecast-temperature-min">
-      12°
+      ${Math.round(forecastDay.temp.min)}°
   </span> 
   </div>
   </div>
@@ -57,7 +68,7 @@ function displayForecast(response) {
   
   function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey ="5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.la}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
   }
